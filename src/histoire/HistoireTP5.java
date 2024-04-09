4c7c8c2b41514fd9578fde5fc6876c0a677b03c3
Timothe Viala <vlt4764a@ensfsi.onmicrosoft.com>
2024-04-09 08:56:54 +0200

    les humains ont des connaissances

---
+++ src/histoire/HistoireTP5.java
@@ -0,0 +1,25 @@
+package histoire;
+
+import personnages.Commercant;
+import personnages.Ronin;
+import personnages.Yakuza;
+
+public class HistoireTP5 {
+
+	public static void main(String[] args) {
+		Commercant marco = new Commercant("Marco", 20);
+		Commercant chonin = new Commercant("Chonin", 40);
+		Commercant kumi = new Commercant("Kumi", 10);
+		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+		Ronin roro = new Ronin("Roro", "shochu", 60);
+		marco.faireConnaissanceAvec(roro);
+		marco.faireConnaissanceAvec(yaku);
+		marco.faireConnaissanceAvec(chonin);
+		marco.faireConnaissanceAvec(kumi);
+		marco.listerConnaissances();
+		roro.listerConnaissances();
+		yaku.listerConnaissances();
+
+	}
+
+}
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -5,6 +5,9 @@ public class Humain {
 	private String nom;
 	private String boissonFavorite;
 	private int argent;
+	private Humain[] connaissances = new Humain[30];
+	private int maxConnaissances = 30;
+	private int nbConnaissances = 0;
 
 	// méthode
 	public String getNom() {
@@ -52,6 +55,42 @@ public class Humain {
 
 	protected void perteArgent(int perte) {
 		argent -= perte;
+	}
+
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+	}
+
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+
+	private void memoriser(Humain humain) {
+		if (nbConnaissances == maxConnaissances) {
+			for (int i = 0; i < connaissances.length - 1; i++) {
+				connaissances[i] = connaissances[i + 1];
+			}
+			connaissances[maxConnaissances - 1] = humain;
+		} else {
+			connaissances[nbConnaissances] = humain;
+			nbConnaissances += 1;
+		}
+	}
+
+	public void listerConnaissances() {
+		StringBuilder builder = new StringBuilder("(" + getNom() + ") -");
+		builder.append("Je connnais beaucoup de monde dont:");
+		for (int i = 0; i < nbConnaissances; i++) {
+			if (i != 0) {
+				builder.append(", ");
+			}
+			builder.append(connaissances[i].getNom());
+
+		}
+		System.out.println(builder + ".");
 	}
 
 	// constructeur
